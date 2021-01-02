e56a35300f093421947a173370ee216766580d64
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2021-01-02 15:07:26 +0700

    card

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,12 +6,13 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   IconButton(
@@ -20,8 +21,11 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              SizedBox(height: 32),
-              RichText(
+            ),
+            SizedBox(height: 32),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: RichText(
                 text: TextSpan(
                   style: TextStyle(
                     fontSize: 24,
@@ -38,39 +42,178 @@ class HomeScreen extends StatelessWidget {
                   ],
                 ),
               ),
-              SizedBox(height: 32),
-              Wrap(
-                spacing: 8,
-                runSpacing: 4,
+            ),
+            SizedBox(height: 32),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  InputChip(
-                    label: Text('Sedan'),
-                    onPressed: () {
-                      Fluttertoast.showToast(msg: 'Sedan');
-                    },
+                  Wrap(
+                    spacing: 8,
+                    runSpacing: 4,
+                    children: [
+                      InputChip(
+                        label: Text(
+                          'Sedan',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        onPressed: () {
+                          Fluttertoast.showToast(msg: 'Sedan');
+                        },
+                      ),
+                      InputChip(
+                        label: Text(
+                          'Pick Up',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        onPressed: () {
+                          Fluttertoast.showToast(msg: 'Pick Up');
+                        },
+                      ),
+                      InputChip(
+                        label: Text(
+                          'Automatic',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        onPressed: () {
+                          Fluttertoast.showToast(msg: 'Automatic');
+                        },
+                      ),
+                    ],
                   ),
-                  InputChip(
-                    label: Text('Pick Up'),
+                  IconButton(
+                    icon: Icon(Icons.tune),
                     onPressed: () {
-                      Fluttertoast.showToast(msg: 'Pick Up');
+                      Fluttertoast.showToast(msg: 'Preferences');
                     },
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 16),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                children: [
+                  AspectRatio(
+                    aspectRatio: 3 / 2,
+                    child: Image.network(
+                      'https://placekitten.com/400/300',
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  InputChip(
-                    label: Text('Automatic'),
-                    onPressed: () {
-                      Fluttertoast.showToast(msg: 'Automatic');
-                    },
+                  SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 4,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RichText(
+                              text: TextSpan(
+                                style: TextStyle(
+                                  color: Colors.grey.shade800,
+                                  fontWeight: FontWeight.w600,
+                                  fontFamily: 'Montserrat',
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: "Toyota Avanza",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: " 2020",
+                                    style: TextStyle(
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 4),
+                            Row(
+                              children: [
+                                Text(
+                                  '4x dipakai',
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                                SizedBox(width: 4),
+                                Icon(
+                                  Icons.star,
+                                  color: Theme.of(context).primaryColor,
+                                  size: 20,
+                                ),
+                                Icon(
+                                  Icons.star,
+                                  color: Theme.of(context).primaryColor,
+                                  size: 20,
+                                ),
+                                Icon(
+                                  Icons.star,
+                                  color: Theme.of(context).primaryColor,
+                                  size: 20,
+                                ),
+                                Icon(
+                                  Icons.star,
+                                  color: Theme.of(context).primaryColor,
+                                  size: 20,
+                                ),
+                                Icon(
+                                  Icons.star_border,
+                                  color: Theme.of(context).primaryColor,
+                                  size: 20,
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            RichText(
+                              text: TextSpan(
+                                style: TextStyle(
+                                  color: Colors.grey.shade800,
+                                  fontWeight: FontWeight.w600,
+                                  fontFamily: 'Montserrat',
+                                ),
+                                children: [
+                                  TextSpan(
+                                    text: "\$ ",
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: "59",
+                                    style: TextStyle(
+                                      fontSize: 24,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Text(
+                              'per day',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
