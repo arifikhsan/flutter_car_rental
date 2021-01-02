18fd3287a0da77c424fed2d8b1f79bc81edc9b58
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2021-01-02 15:13:22 +0700

    uwu

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -22,7 +22,7 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(height: 32),
+            SizedBox(height: 16),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
               child: RichText(
@@ -43,7 +43,7 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(height: 32),
+            SizedBox(height: 16),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Row(
@@ -92,124 +92,132 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             SizedBox(height: 16),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Column(
-                children: [
-                  AspectRatio(
-                    aspectRatio: 3 / 2,
-                    child: Image.network(
-                      'https://placekitten.com/400/300',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    children: [
-                      Expanded(
-                        flex: 4,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            RichText(
-                              text: TextSpan(
-                                style: TextStyle(
-                                  color: Colors.grey.shade800,
-                                  fontWeight: FontWeight.w600,
-                                  fontFamily: 'Montserrat',
-                                ),
-                                children: [
-                                  TextSpan(
-                                    text: "Toyota Avanza",
-                                    style: TextStyle(
-                                      fontSize: 24,
-                                    ),
-                                  ),
-                                  TextSpan(
-                                    text: " 2020",
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            SizedBox(height: 4),
-                            Row(
-                              children: [
-                                Text(
-                                  '4x dipakai',
-                                  style: TextStyle(fontSize: 12),
-                                ),
-                                SizedBox(width: 4),
-                                Icon(
-                                  Icons.star,
-                                  color: Theme.of(context).primaryColor,
-                                  size: 20,
-                                ),
-                                Icon(
-                                  Icons.star,
-                                  color: Theme.of(context).primaryColor,
-                                  size: 20,
-                                ),
-                                Icon(
-                                  Icons.star,
-                                  color: Theme.of(context).primaryColor,
-                                  size: 20,
-                                ),
-                                Icon(
-                                  Icons.star,
-                                  color: Theme.of(context).primaryColor,
-                                  size: 20,
-                                ),
-                                Icon(
-                                  Icons.star_border,
-                                  color: Theme.of(context).primaryColor,
-                                  size: 20,
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
+            FlatButton(
+              onPressed: () {},
+              padding: EdgeInsets.all(16),
+              child: Container(
+                child: Column(
+                  children: [
+                    AspectRatio(
+                      aspectRatio: 3 / 2,
+                      child: Image.network(
+                        'https://placekitten.com/400/300',
+                        fit: BoxFit.cover,
                       ),
-                      Expanded(
-                        child: Column(
-                          children: [
-                            RichText(
-                              text: TextSpan(
-                                style: TextStyle(
-                                  color: Colors.grey.shade800,
-                                  fontWeight: FontWeight.w600,
-                                  fontFamily: 'Montserrat',
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 4,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              RichText(
+                                text: TextSpan(
+                                  style: TextStyle(
+                                    color: Colors.grey.shade800,
+                                    fontWeight: FontWeight.w700,
+                                    fontFamily: 'Montserrat',
+                                  ),
+                                  children: [
+                                    TextSpan(
+                                      text: "Toyota Avanza",
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: " 2020",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                      ),
+                                    ),
+                                  ],
                                 ),
+                              ),
+                              SizedBox(height: 4),
+                              Row(
                                 children: [
-                                  TextSpan(
-                                    text: "\$ ",
+                                  Text(
+                                    '4x used',
                                     style: TextStyle(
                                       fontSize: 12,
+                                      color: Colors.grey.shade700,
                                     ),
                                   ),
-                                  TextSpan(
-                                    text: "59",
-                                    style: TextStyle(
-                                      fontSize: 24,
-                                    ),
+                                  SizedBox(width: 4),
+                                  Icon(
+                                    Icons.star,
+                                    color: Theme.of(context).primaryColor,
+                                    size: 20,
+                                  ),
+                                  Icon(
+                                    Icons.star,
+                                    color: Theme.of(context).primaryColor,
+                                    size: 20,
+                                  ),
+                                  Icon(
+                                    Icons.star,
+                                    color: Theme.of(context).primaryColor,
+                                    size: 20,
+                                  ),
+                                  Icon(
+                                    Icons.star,
+                                    color: Theme.of(context).primaryColor,
+                                    size: 20,
+                                  ),
+                                  Icon(
+                                    Icons.star_border,
+                                    color: Theme.of(context).primaryColor,
+                                    size: 20,
                                   ),
                                 ],
-                              ),
-                            ),
-                            Text(
-                              'per day',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ],
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                        Expanded(
+                          child: Column(
+                            children: [
+                              RichText(
+                                text: TextSpan(
+                                  style: TextStyle(
+                                    color: Colors.grey.shade800,
+                                    fontWeight: FontWeight.w600,
+                                    fontFamily: 'Montserrat',
+                                  ),
+                                  children: [
+                                    TextSpan(
+                                      text: "\$ ",
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: "59",
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.w800,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Text(
+                                'per day',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.grey.shade700,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
